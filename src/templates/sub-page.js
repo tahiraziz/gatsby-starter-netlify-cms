689a2435459e560aae03d05a0c1585e8b48f760f
Tahir Aziz <tahir.aziz4@gmail.com>
2018-11-15 21:01:21 -0600

    more fixes on subpage query

--- src/templates/sub-page.js
+++ src/templates/sub-page.js
@@ -1,74 +1,56 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 // import PageHeader from
 
 export const SubPageTemplate = ({
     title,
     coverImage,
-    sections
+    copy
 }) => (
         <main>
             <section class="letter">
                 <div class="copy">
-                    {
-                        sections.map(section => (
-                            <div>
-                                <h1>
-                                    {section.header}
-                                </h1>
-                                <div>
-                                    {section.markdownRemark.html}
-                                </div>
-                            </div>
-                        )
-                        )
-                    }
+                    {copy}
                 </div>
             </section>
         </main>
     )
 
-SubPageTemplate.propTypes = {
-    title: PropTypes.string,
-    coverImage: PropTypes.string,
-    sections: PropTypes.array
-}
+// SubPageTemplate.propTypes = {
+//     title: PropTypes.string,
+//     coverImage: PropTypes.string,
+//     copy: PropTypes.object
+// }
 
 const SubPage = ({ data }) => {
-    const { frontmatter } = data.markdownRemark
+    const { queryData } = data.markdownRemark
 
     return (
         <SubPageTemplate
-            title={frontmatter.title}
-            coverImage={frontmatter.coverImage}
-            sections={frontmatter.sections}
+            title={queryData.frontmatter.title}
+            coverImage={queryData.frontmatter.coverImage}
+            copy={queryData.html}
         />
     )
 }
 
-SubPage.propTypes = {
-    data: PropTypes.shape({
-        markdownRemark: PropTypes.shape({
-            frontmatter: PropTypes.object,
-        }),
-    }),
-}
+// SubPage.propTypes = {
+//     data: PropTypes.shape({
+//         markdownRemark: PropTypes.object,
+//     }),
+// }
 
 export default SubPage
 
 export const subPageQuery = graphql`
-  query SubPage($id: String!) {
-    markdownRemark(id: { eq: $id }) {
-      frontmatter {
-        coverImage
-        title
-        sections {
-            header,
-            markdownRemark {
-                html
+    query SubPage($id: String!) {
+        markdownRemark(id: { eq: $id }) {
+            id
+            html
+            frontmatter {
+                title
+                coverImage
             }
         }
-      }
     }
-  }
 `
